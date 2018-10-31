65df66a4b70ba9cb335a03298dacbad34071c13f
swain <m109x120@163.com>
2018-10-31 18:53:13 +0800

    登录组件完成

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,27 +1,66 @@
 <template>
-<div>
-    <el-radio-group v-model="labelPosition" size="small">
-        <el-radio-button label="left">左对齐</el-radio-button>
-        <el-radio-button label="right">右对齐</el-radio-button>
-        <el-radio-button label="top">顶部对齐</el-radio-button>
-    </el-radio-group>
-    <div style="margin: 20px;"></div>
-    <el-form :label-position="labelPosition" label-width="80px" :model="formLabelAlign">
-        <el-form-item label="名称">
-            <el-input v-model="formLabelAlign.name"></el-input>
-        </el-form-item>
-        <el-form-item label="活动区域">
-            <el-input v-model="formLabelAlign.region"></el-input>
-        </el-form-item>
-        <el-form-item label="活动形式">
-            <el-input v-model="formLabelAlign.type"></el-input>
-        </el-form-item>
-    </el-form> 
-</template> 
+<div class="login-wrap">
+  <el-form
+  class="login-form"
+  label-width="80px"
+  >
+    <h1>用户登录</h1>
+    <el-form-item
+    label="用户名"
+    v-model='formdata.username'
+    placeholder="请输入用户名"
+    >
+      <el-input></el-input>
+    </el-form-item>
+    <el-form-item
+    label="密码"
+    v-model='formdata.password'
+    placeholder="请输入密码"
+    >
+      <el-input></el-input>
+    </el-form-item>
+    <el-button
+    type="primary"
+    @click='login'
+    class="login-button"
+    >登录</el-button>
+  </el-form>
 </div>
-  
+</template>
+
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      formdata: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    login () {
+      console.log('你点击了登录')
+    }
+  }
+}
 </script>
+
 <style>
+.login-wrap{
+  background-color: #324152;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.login-wrap .login-form{
+  background-color: #fff;
+  width: 400px;
+  padding: 30px;
+  border-radius: 5px;
+}
+.login-wrap .login-form .login-button{
+  width: 100%;
+}
 </style>
